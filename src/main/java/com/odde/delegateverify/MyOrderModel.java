e4d8db8271d40888b52a0e11de1b397c074568d3
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-06-06 12:09:41 +0800

    write order model tests

--- src/main/java/com/odde/delegateverify/MyOrderModel.java
+++ src/main/java/com/odde/delegateverify/MyOrderModel.java
@@ -1,5 +1,6 @@
 package com.odde.delegateverify;
 
+import java.time.Clock;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.function.Consumer;
@@ -7,15 +8,22 @@ import java.util.function.Predicate;
 
 public class MyOrderModel implements OrderModel {
     private final Repository repository;
+    private final Clock clock;
 
     public MyOrderModel(Repository repository) {
         this.repository = repository;
+        clock = null;
+    }
+
+    public MyOrderModel(Repository repository, Clock clock) {
+        this.repository = repository;
+        this.clock = clock;
     }
 
     @Override
     public void save(Order order, Consumer<Order> insertCallback, Consumer<Order> updateCallback) {
         if (!repository.isExist(order)) {
-            if (LocalDate.now().getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
+            if (LocalDate.now(clock).getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
                 order.setAmount(order.getAmount() + 100);
             }
             repository.insert(order);
--- src/test/java/com/odde/delegateverify/OrderModelTest.java
+++ src/test/java/com/odde/delegateverify/OrderModelTest.java
@@ -2,21 +2,80 @@ package com.odde.delegateverify;
 
 import org.junit.Test;
 
-import static org.mockito.Mockito.mock;
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.function.Consumer;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class OrderModelTest {
 
     private Repository repository = mock(Repository.class);
+    Clock stubClock = stubClock(LocalDate.of(2018, 6, 9));
+    MyOrderModel myOrderModel = new MyOrderModel(repository, stubClock);
+    Order order = new Order();
+    Consumer mockInsertCallback = mock(Consumer.class);
+    Consumer mockUpdateCallback = mock(Consumer.class);
 
     @Test
     public void insert_order() {
-        //TODO
-        MyOrderModel myOrderModel = new MyOrderModel(repository);
+        givenOrderExists(false);
+
+        myOrderModel.save(order, mockInsertCallback, mockUpdateCallback);
+
+        verify(repository).insert(order);
+        verify(mockInsertCallback).accept(order);
     }
-    
+
     @Test
     public void update_order() {
-        //TODO
-        MyOrderModel myOrderModel = new MyOrderModel(repository);
+        givenOrderExists(true);
+
+        myOrderModel.save(order, mockInsertCallback, mockUpdateCallback);
+
+        verify(repository).update(order);
+        verify(mockUpdateCallback).accept(order);
     }
+
+    @Test
+    public void insert_order_on_sunday() {
+        givenTodayIsSunday();
+        givenOrderExists(false);
+
+        order.setAmount(50);
+        myOrderModel.save(order, mockInsertCallback, mockUpdateCallback);
+
+        assertEquals(150, order.getAmount());
+    }
+
+    @Test
+    public void insert_order_not_on_sunday() {
+        givenTodayIsNotSunday();
+        givenOrderExists(false);
+
+        order.setAmount(50);
+        myOrderModel.save(order, mockInsertCallback, mockUpdateCallback);
+
+        assertEquals(50, order.getAmount());
+    }
+
+    private void givenTodayIsSunday() {
+        myOrderModel = new MyOrderModel(repository, stubClock(LocalDate.of(2018, 6, 10)));
+    }
+
+    private void givenTodayIsNotSunday() {
+        myOrderModel = new MyOrderModel(repository, stubClock(LocalDate.of(2018, 6, 9)));
+    }
+
+    private void givenOrderExists(boolean b) {
+        when(repository.isExist(any(Order.class))).thenReturn(b);
+    }
+
+    private Clock stubClock(LocalDate sunday) {
+        return Clock.fixed(sunday.atStartOfDay(ZoneId.systemDefault()).toInstant(), ZoneId.systemDefault());
+    }
+
 }
